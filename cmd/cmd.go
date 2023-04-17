36cfab30b95293bce3c15520e369e6200039b56d
yunsang-jeong <imyunsang@kakao.com>
2023-04-18 01:23:57 +0900

    코드 블럭 정리

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,12 +1,64 @@
 package cmd
 
-type stringFlag struct {
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+type flag struct {
+	_type        string
 	shorten      string
-	defaultValue string
+	defaultValue interface{}
 	description  string
 	requirement  bool
 }
 
-const (
-	BucketName = "bucket-name"
-)
+func cobraFlagRegister(c *cobra.Command, flags map[string]flag) {
+	for name, flag := range flags {
+
+		switch flag._type {
+		case "string":
+			if flag.defaultValue != nil {
+				c.Flags().StringP(
+					name,
+					flag.shorten,
+					flag.defaultValue.(string),
+					flag.description,
+				)
+			} else {
+				c.Flags().StringP(
+					name,
+					flag.shorten,
+					"",
+					flag.description,
+				)
+			}
+			if flag.requirement {
+				c.MarkFlagRequired(name)
+			}
+
+		case "list":
+			if flag.defaultValue != nil {
+				c.Flags().StringSliceP(
+					name,
+					flag.shorten,
+					flag.defaultValue.([]string),
+					flag.description,
+				)
+			} else {
+				c.Flags().StringSliceP(
+					name,
+					flag.shorten,
+					[]string{},
+					flag.description,
+				)
+			}
+			if flag.requirement {
+				c.MarkFlagRequired(name)
+			}
+		default:
+			panic(fmt.Sprintf("unsupported flag type: %s", flag._type))
+		}
+	}
+}
--- cmd/root.go
+++ cmd/root.go
@@ -6,14 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type flag struct {
-	_type        string
-	shorten      string
-	defaultValue interface{}
-	description  string
-	requirement  bool
-}
-
 var RootCmd = &cobra.Command{
 	Use:           "tpm",
 	Short:         "Terraform provider mirror with s3",
@@ -35,35 +27,5 @@ func Execute() {
 
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
-	}
-}
-
-func cobraFlagRegister(c *cobra.Command, flags map[string]flag) {
-	for name, flag := range flags {
-
-		switch flag._type {
-		case "string":
-			c.Flags().StringP(
-				name,
-				flag.shorten,
-				flag.defaultValue.(string),
-				flag.description,
-			)
-			if flag.requirement {
-				c.MarkFlagRequired(name)
-			}
-		case "list":
-			c.Flags().StringSliceP(
-				name,
-				flag.shorten,
-				flag.defaultValue.([]string),
-				flag.description,
-			)
-			if flag.requirement {
-				c.MarkFlagRequired(name)
-			}
-		default:
-			panic(fmt.Sprintf("unsupported flag type: %s", flag._type))
-		}
 	}
 }
